df292a3515b61fe750c2023c22375ab2b076f41c
MarcoLoWaferSystems <marcolo@wafersystems.com>
2022-08-01 15:42:11 +0800

    已重新安裝 Font Awesome

--- src/main.js
+++ src/main.js
@@ -9,9 +9,21 @@ import './assets/css/style.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-//import { library } from '@fortawesome/fontawesome-svg-core'
-//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-//import { faExclamation} from '@fortawesome/free-solid-svg-icons'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faExclamation } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faExclamation);
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 new Vue({
