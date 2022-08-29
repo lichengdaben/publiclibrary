0d6680f524ca072f97e301430d54c29058efaf11
MarcoLoWaferSystems <marcolo@wafersystems.com>
2022-08-29 18:01:38 +0800

    0829

--- src/components/DateTimeChooseTable.vue
+++ src/components/DateTimeChooseTable.vue
@@ -3,83 +3,121 @@
         <div class="p-order">
             <div class="firstHeader">
                 <div class="firstfont">DATE OF USE</div>
-                <div class="secondfont">{{currentDateTime()}}</div>
+                <div class="secondfont" style="padding: 10px 0px 0px 0px; margin: 0px;">{{ currentDateTime }}</div>
             </div>
             <div class="dateOfTime">
-            <div v-for="data in this.dateOfUse" :key="data.yearAndMonth">
-                <b-button-group class="Calendar-group" style="margin: 10px;">
-                    <b-button class="Calendar-primary" @click="click(data.dayAndWeek)">
-                    <div>
-                        <span class="Cal-Month">{{data.currentDay}}</span>
-                        <span class="Cal-Date">{{data.dayAndWeek}}</span>
-                    </div>
-                     <div>
-                         <div v-if="data.close"  class="secondHeader">Close</div>
-                        <div v-if="!data.holiday" class="secondHeader">Holiday</div>
-                        <div v-show="chooseDate.includes(data.dayAndWeek)" class="secondHeader">Selected</div>
-                    </div>
-                    </b-button>
-                </b-button-group>
+                <div v-for="data in this.dateOfUse" :key="data.yearAndMonth">
+                    <b-button-group class="Calendar-group" style="margin: 10px;">
+                        <b-button v-if="data.close" class="Calendar-primary" style="margin: 0px 10px; background-color: #545B62; color: white;">
+                            <div>
+                                <!--<span class="Cal-Month">{{ data.currentDay }}</span>-->
+                                <span class="Cal-Date">{{ data.dayAndWeek }}</span>
+                            </div>
+                            <div>
+                                <div style="background-color: #545B62;">Closed</div>
+                            </div>
+                        </b-button>
+                        <b-button v-else-if="!data.close" class="Calendar-primary" @click="click(data.dayAndWeek)" style="margin: 0px 10px;">
+                            <div>
+                                <!--<span class="Cal-Month">{{ data.currentDay }}</span>-->
+                                <span class="Cal-Date">{{ data.dayAndWeek }}</span>
+                            </div>
+                            <div>
+                                <div v-if="chooseDate.includes(data.dayAndWeek)" class="secondHeader">Selected</div>
+                                <div v-else-if="data.holiday" class="secondHeader">Holiday</div>
+                            </div>
+                        </b-button>
+                    </b-button-group>
+                </div>
             </div>
-             </div>
         </div>
+
         <div class="p-order">
             <div class="firstHeader">
                 <div class="firstfont">SESSION DURATION:</div>
-                <div class="secondfont">Usage the thime</div>
             </div>
-            <b-button-group class="Calendar-group">
-                <b-button cols="3" class="ChooseTime-primary" variant="primary"  @click="clickHour(1)">
-                    <span class="Ct-Time">1 Hour</span>
+            <b-button-group class="Calendar-group" style="padding: 10px 0px;">
+                <b-button cols="3" class="ChooseTime-primary" style="border: 1px solid grey;" variant="primary" @click="clickHour(1)">
+                    <span class="Ct-Time" style="width: 100%">1 Hour</span>
                 </b-button>
-                <b-button class="ChooseTime-outline" variant="outline-dark"  @click="clickHour(2)">
-                    <span class="Ct-Time" >2 Hour</span>
+                <b-button cols="3" class="ChooseTime-primary" style="border: 1px solid grey;" variant="primary" @click="clickHour(2)">
+                    <span class="Ct-Time" style="width: 100%">2 Hours</span>
                 </b-button>
             </b-button-group>
         </div>
+
         <div class="p-order">
             <div class="firstHeader clearfix">
-                <div class="firstfont">TIME PERIOD:</div>
+                <div class="firstfont">TIME SLOT:</div>
             </div>
             <b-button-group class="TimePeriod-group">
-                <ul class="clearfix">
+                <table>
+                    <thead>
+                        <tr>
+                            <th colspan="4">Morning</th>
+                            <th colspan="5">Afternoon</th>
+                            <th colspan="5">Night</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td v-for="time in morningPeriod" :key="time.id" v-bind:id="'timeRange' + time">
+                                <div class="timeRange">{{ time.name }}</div>
+                                <div align="center">
+                                    <input type="checkbox" :name="time.name" :value="time.name" class="checkbox" />
+                                </div>
+                            </td>
+                            <td v-for="time in afternoonPeriod" :key="time.id" v-bind:id="'timeRange' + time">
+                                <div class="timeRange">{{ time.name }}</div>
+                                <div align="center">
+                                    <input type="checkbox" :name="time.name" :value="time.name" class="checkbox" />
+                                </div>
+                            </td>
+                            <td v-for="time in nightPeriod" :key="time.id" v-bind:id="'timeRange' + time">
+                                <div class="timeRange">{{ time.name }}</div>
+                                <div align="center">
+                                    <input type="checkbox" :name="time.name" :value="time.name" class="checkbox" />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!--<ul class="clearfix">
                     <li>
                         <div class="secondfont">Morning</div>
-                        <b-button v-for='(product, index) in MorningPeriod' :key='product.id' class="TimePeriod-outline"
-                            variant="outline-dark">
-                            <span class="Tp-Time"><span style="display: none;">{{ index + 1}}
-                                </span>{{ product.name }}</span>
+                        <b-button v-for='(product, index) in MorningPeriod' :key='product.id' class="TimePeriod-outline" variant="outline-dark">
+                            <span class="Tp-Time">
+                                <span style="display: none;">{{ index + 1 }}</span>{{ product.name }}
+                            </span>
                             <input type="checkbox" checked="checked" class="checkedboxstyle">
                         </b-button>
                     </li>
                     <li>
-                        <div class="secondfont">Afternoon </div>
-                        <b-button v-for='(product, index) in AfternoonPeriod' :key='product.id'
-                            class="TimePeriod-outline" variant="outline-dark">
-                            <span class="Tp-Time"><span style="display: none;">{{ index + 1}}
-                                </span>{{ product.name }}</span>
+                        <div class="secondfont">Afternoon</div>
+                        <b-button v-for='(product, index) in AfternoonPeriod' :key='product.id' class="TimePeriod-outline" variant="outline-dark">
+                            <span class="Tp-Time">
+                                <span style="display: none;">{{ index + 1 }}</span>{{ product.name }}
+                            </span>
                             <input type="checkbox" checked="checked" class="checkedboxstyle ">
                         </b-button>
                     </li>
                     <li>
-                        <div class="secondfont">Night </div>
-                        <b-button v-for='(product, index) in NightPeriod' :key='product.id' class="TimePeriod-outline"
-                            variant="outline-dark">
-                            <span class="Tp-Time"><span style="display: none;">{{ index + 1}}
-                                </span>{{ product.name }}</span>
+                        <div class="secondfont">Night</div>
+                        <b-button v-for='(product, index) in NightPeriod' :key='product.id' class="TimePeriod-outline" variant="outline-dark">
+                            <span class="Tp-Time">
+                                <span style="display: none;">{{ index + 1 }}</span>{{ product.name }}
+                            </span>
                             <input type="checkbox" checked="checked" class="checkedboxstyle ">
                         </b-button>
                     </li>
-                </ul>
+                </ul>-->
             </b-button-group>
         </div>
     </b-container>
 </template>
 
 <script>
-    import {
-        getDateOfUse
-    } from '@/service/test.js'
+    import { getDateOfUse } from '@/service/test.js'
     export default {
         name: 'DateTimeChooseTable',
         data() {
@@ -88,6 +126,10 @@
                 dateOfUse: null,
                 isShow:false,
                 chooseDate: [],
+                currentDateTime: null,
+                morningPeriod: null,
+                afternoonPeriod: null,
+                nightPeriod: null
             }
         },
         props: {
@@ -97,37 +139,33 @@
             async click(dayAndWeek) {
                 this.$store.commit('selectedDate',dayAndWeek);
                 this.$store.state.selectedDate=dayAndWeek
-                console.log( this.$store.state.selectedDate)
-                    if (!this.chooseDate.includes(dayAndWeek)) {
-                        this.chooseDate.push(dayAndWeek)
+                console.log(this.$store.state.selectedDate)
+                if (!this.chooseDate.includes(dayAndWeek)) {
+                    this.chooseDate.push(dayAndWeek)
+                } else {
+                    let index = this.chooseDate.indexOf(dayAndWeek);
+                    if (index !== -1) {
+                        this.chooseDate.splice(index, 1);
                     }
-                    else {
-                        let index = this.chooseDate.indexOf(dayAndWeek);
-                        if (index !== -1) {
-                            this.chooseDate.splice(index, 1);
-                        }
-                    }
-                },
-            async  clickHour(hourTime){
-                if(hourTime==1){
-                    this.$store.commit('selectedHour',hourTime);
-                    this.$store.state.selectedHour=hourTime
-                    console.log(this.$store.state.selectedHour)
-                }else if(hourTime==2){
-                    this.$store.commit('selectedHour',hourTime);
-                    this.$store.state.selectedHour=hourTime
-                    console.log(this.$store.state.selectedHour)
                 }
             },
-            async clickDate(date){
+
+            async clickHour(hourTime) {
+                this.$store.commit('selectedHour', hourTime);
+                //this.$store.state.selectedHour = hourTime;
+                //console.log(this.$store.state.selectedHour);
+            },
+
+            async clickDate(date) {
                 this.$store.commit('selectedDate',date);
                 this.$store.state.selectedDate=date
             },
-            async currentDateTime() {
+
+            getCurrentDateTime() {
                 const current = new Date();
-                const date = current.getDate() + '-' + (current.getMonth() + 1) + '-' + current.getFullYear();
-                const dateTime = date;
-                return dateTime;
+                const monthArray = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+                const date = monthArray[current.getMonth()] + ' ' + current.getFullYear();
+                return date;
             },
             // currentHour() {
             //     const current = new Hour();
@@ -137,78 +175,88 @@
             // },
         },
         async created() {
-            console.log( 'aaaaaaa' )
             this.dateOfUse = (await getDateOfUse(1)).data.data;
-            console.log( this.dateOfUse )
+            this.clickHour(1);
+            console.log(this.dateOfUse);
+        },
+        async mounted() {
+            this.currentDateTime = this.getCurrentDateTime();
+            this.morningPeriod = MorningPeriod.value;
+            this.afternoonPeriod = AfternoonPeriod.value;
+            this.nightPeriod = NightPeriod.value;
         }
     }
-import {
-            ref
-        } from 'vue'
-        const MorningPeriod = ref([{
-                id: 0,
-                name: '9:00'
-            },
-            {
-                id: 1,
-                name: '10:00'
-            },
-            {
-                id: 2,
-                name: '11:00'
-            },
-            {
-                id: 3,
-                name: '12:00'
-            }
-        ])
-        console.log(MorningPeriod);
-        /*const AfternoonPeriod = ref([{
-                id: 4,
-                name: '13:00'
-            },
-            {
-                id: 5,
-                name: '14:00'
-            },
-            {
-                id: 6,
-                name: '15:00'
-            },
-            {
-                id: 7,
-                name: '16:00'
-            },
-            {
-                id: 8,
-                name: '17:00'
-            }
-        ])
-        const NightPeriod = ref([{
-                id: 9,
-                name: '18:00'
-            },
-            {
-                id: 10,
-                name: '19:00'
-            },
-            {
-                id: 11,
-                name: '20:00'
-            },
-            {
-                id: 12,
-                name: '21:00'
-            }
-        ]) */
+
+    import { ref } from 'vue'
+
+    const MorningPeriod = ref([
+        {
+            id: 0,
+            name: '9:00'
+        },
+        {
+            id: 1,
+            name: '10:00'
+        },
+        {
+            id: 2,
+            name: '11:00'
+        },
+        {
+            id: 3,
+            name: '12:00'
+        }
+    ]);
+
+    const AfternoonPeriod = ref([{
+            id: 4,
+            name: '13:00'
+        },
+        {
+            id: 5,
+            name: '14:00'
+        },
+        {
+            id: 6,
+            name: '15:00'
+        },
+        {
+            id: 7,
+            name: '16:00'
+        },
+        {
+            id: 8,
+            name: '17:00'
+        }
+    ]);
+
+    const NightPeriod = ref([
+        {
+            id: 9,
+            name: '18:00'
+        },
+        {
+            id: 10,
+            name: '19:00'
+        },
+        {
+            id: 11,
+            name: '20:00'
+        },
+        {
+            id: 12,
+            name: '21:00'
+        }
+    ]);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 .btn{
     display: flex;
     align-items:center;
-    flex-direction:row;
+    flex-direction: column;
     font-weight: 400;
     color: #212529;
     text-align: center;
@@ -221,13 +269,39 @@ import {
     padding: 0.375rem 0.75rem;
     font-size: 1rem;
     line-height: 1.5;
-    border-radius: 0.25rem;
+    border-radius: 0.25rem !important;
     transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
+
 .dateOfTime{
-    width:60px;
     display: flex;
     align-items:center;
     flex-direction:row;
 }
+
+.TimePeriod-group table {
+    font-weight: bold;
+}
+
+.TimePeriod-group th {
+    padding: 10px 0px;
+    font-size: medium;
+}
+
+.TimePeriod-group td {
+    border-left: 1px solid grey;
+    border-right: 1px solid grey; 
+    padding: 0px 10px;
+}
+
+.timeRange {
+    padding: 10px 0px;
+}
+
+.checkbox {
+    accent-color: #096BCC;
+    width: 25px;
+    height: 25px;
+}
+
 </style>
--- src/views/Booking/DateTimeChoose.vue
+++ src/views/Booking/DateTimeChoose.vue
@@ -29,45 +29,49 @@
                     </ul>
                 </b-col>
                 <b-col cols="9" class="right-content">
-                     <div ref="parentSidebar" id="parentx">
-                            <vs-sidebar :parent="$refs.parentSidebar" default-index="1" 
-                            class="sidebarx" pacer v-model="active">
-                               <div class="header-sidebar">
-                                  <div class="settingtitle">Feature</div>
-                                    <div  v-for="feature in  workstationFeature" :key="feature.featureName" >
-                            <input type="radio" id="workstationFeature" name="fav_language" value="workstationFeature" v-on:click="clickWorkStationFeature(feature.featureName)">
-                            <label for="workstationFeature">
-                                <div class="thirdfont"> {{feature.featureName}}<br></div>
-                            </label>
+                    <div ref="parentSidebar" id="parentx">
+                        <vs-sidebar :parent="$refs.parentSidebar" default-index="1" class="sidebarx" pacer v-model="active">
+                            <div class="header-sidebar">
+                                <div class="settingtitle">Feature</div>
+                                <div v-for="feature in workstationFeature" :key="feature.featureName">
+                                    <input type="radio" id="workstationFeature" name="fav_language" value="workstationFeature" v-on:click="clickWorkStationFeature(feature.featureName)">
+                                    <label for="workstationFeature">
+                                        <div class="thirdfont"> {{feature.featureName}}<br></div>
+                                    </label>
                                 </div>
+                            </div>
+                            <div class="header-sidebar">
+                                <div class="settingtitle">Language</div>
+                                <div v-for="Language in  workstationLanguage" :key="Language.languageId" >
+                                    <input type="radio" id="workstationLanguage" name="fav_language" value="workstationLanguage" v-on:click="clickWorkStationLanguage(Language.languageName)">
+                                    <label for="workstationLanguage">
+                                        <div class="thirdfont"> {{Language.languageName}}<br></div>
+                                    </label>
                                 </div>
-                                 <div class="header-sidebar">
-                                    <div class="settingtitle">Language</div>
-                            <div  v-for="Language in  workstationLanguage" :key="Language.languageId" >
-                            <input type="radio" id="workstationLanguage" name="fav_language" value="workstationLanguage" v-on:click="clickWorkStationLanguage(Language.languageName)">
-                            <label for="workstationLanguage">
-                                <div class="thirdfont"> {{Language.languageName}}<br></div>
-                            </label>
-                                </div>
-                                </div>
-                            </vs-sidebar>
-                        </div>
-                        <DateTimeChooseTable />
-                    <div>
+                            </div>
+                        </vs-sidebar>
                     </div>
+                    <DateTimeChooseTable />
+                    <div></div>
                 </b-col>
             </b-row>
         </b-container>
+        <PageFooter />
     </div>
 </template>
+
 <script>
     import DateTimeChooseTable from '/src/components/DateTimeChooseTable.vue'
     import  {workstationLanguage,workstationFeature} from '@/service/test.js'
     import NavBar from '@/components/NavBar.vue'
+    import PageFooter from '/src/components/PageFooter.vue'
+
     export default {
         name: 'DateTimeChoose',
         components: {
-            DateTimeChooseTable,NavBar 
+            DateTimeChooseTable,
+            NavBar,
+            PageFooter
         },
         data() {
             return {
